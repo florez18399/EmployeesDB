f73e6cd14ac694cb1e6d7d274e35935b8d2e7f0b
Andres Florez <andres.florez03@uptc.edu.co>
2018-05-25 18:43:55 -0500

    Clase departamento terminada

--- src/models/Department.java
+++ src/models/Department.java
@@ -1,5 +1,51 @@
 package models;
 
 public class Department {
+	private int idDepartment;
+	private String nameDepartment;
+	private String managerNumber;
+	private String locationName;
+
+	public Department(int idDepartment, String nameDepartment, String managerNumber, String locationName) {
+		this.idDepartment = idDepartment;
+		this.nameDepartment = nameDepartment;
+		this.managerNumber = managerNumber;
+		this.locationName = locationName;
+	}
+
+	public Department() {
+	}
+
+	public int getIdDepartment() {
+		return idDepartment;
+	}
+
+	public void setIdDepartment(int idDepartment) {
+		this.idDepartment = idDepartment;
+	}
+
+	public String getNameDepartment() {
+		return nameDepartment;
+	}
+
+	public void setNameDepartment(String nameDepartment) {
+		this.nameDepartment = nameDepartment;
+	}
+
+	public String getManagerNumber() {
+		return managerNumber;
+	}
+
+	public void setManagerNumber(String managerNumber) {
+		this.managerNumber = managerNumber;
+	}
+
+	public String getLocationName() {
+		return locationName;
+	}
+
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
 
 }
