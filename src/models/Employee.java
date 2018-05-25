3ab551754f3def3aca3f488261457243d0e193f4
Andres Florez <andres.florez03@uptc.edu.co>
2018-05-25 18:56:47 -0500

    Clase empleado terminada

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -1,5 +1,73 @@
 package models;
 
+import java.util.Calendar;
+
 public class Employee {
-	
+	private int idEmployee;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private Calendar hiredDate;
+	private int currentSalary;
+	private Department department;
+
+	public Employee() {
+	}
+
+	public int getIdEmployee() {
+		return idEmployee;
+	}
+
+	public void setIdEmployee(int idEmployee) {
+		this.idEmployee = idEmployee;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Calendar getHiredDate() {
+		return hiredDate;
+	}
+
+	public void setHiredDate(Calendar hiredDate) {
+		this.hiredDate = hiredDate;
+	}
+
+	public int getCurrentSalary() {
+		return currentSalary;
+	}
+
+	public void setCurrentSalary(int currentSalary) {
+		this.currentSalary = currentSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
 }
